a444d964e13dbdc2581eb9a30067b2eab5e4268d
Diego Tolaba <diegotolaba09@gmail.com>
2022-01-13 15:25:55 -0300

    Feat/Added comments section with disqus and resolved suggestions

--- src/js/app.js
+++ src/js/app.js
@@ -47,6 +47,8 @@ $(() => {
   const $newsletterElements = $('.js-newsletter');
   const currentSavedTheme = localStorage.getItem('theme');
 
+  const NEWSLETTER_POPUP_SHOW = 'newsletter_popup_shown';
+
   let fuse = null;
   let submenuIsOpen = false;
   let secondaryMenuTippy = null;
@@ -134,11 +136,11 @@ $(() => {
     const hrefURL = window.location.href;
     const popup = document.querySelector('.popup-wrapper');
     const close = document.querySelector('.popup-close');
-    const lsNewsletter = localStorage.getItem('newsletter');
+    const lsNewsletter = localStorage.getItem(NEWSLETTER_POPUP_SHOW);
     if (hrefURL === `${ghostHost}/` && !lsNewsletter) {
       setTimeout(() => {
         popup.style.display = 'flex';
-        localStorage.setItem('newsletter', 'ok');
+        localStorage.setItem(NEWSLETTER_POPUP_SHOW, 'ok');
       }, 22000);
     }
     window.addEventListener('keyup', e => {
